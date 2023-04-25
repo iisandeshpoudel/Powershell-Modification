ef4895ec56b61357672b1a32950930d30679367b
ThinZin <51796069+iisandeshpoudel@users.noreply.github.com>
2023-04-26 01:02:46 +0545

    Update ThinZin.ps1

--- ThinZin.ps1
+++ ThinZin.ps1
@@ -1,2 +1,14 @@
-#
-Write-Host "Succcccesufullllllll"
+Import-Module oh-my-posh
+Import-Module posh-git
+Set-PoshPrompt -Theme amro
+Import-Module -Name Terminal-Icons
+Import-Module z
+Import-Module PSReadLine
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+#Remove the "#" of the below line and replace the default location for default of powershell startup.
+
+#Set-Location D:\
